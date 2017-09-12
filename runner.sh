4cafbd981b73bfdbc071c3d0ee16cb8e5ee407b6
mmuhaimi <mmuhaimi@asb9700u-b07.csil.sfu.ca>
2017-09-12 16:10:02 -0700

    Add runner with both input

--- runner.sh
+++ runner.sh
@@ -11,5 +11,9 @@ export HADOOP_HOME=/usr/shared/CMPT/big-data/hadoop-2.6.0
 ${JAVA_HOME}/bin/javac -classpath `${HADOOP_HOME}/bin/hadoop classpath` WordCountImproved.java
 ${JAVA_HOME}/bin/jar cf wordcountimproved.jar WordCount*.class
 
-${HADOOP_HOME}/bin/yarn jar wordcountimproved.jar WordCountImproved wordcount-2 output
-head output/part-r-00000
+${HADOOP_HOME}/bin/yarn jar wordcountimproved.jar WordCountImproved wordcount-1 output-1
+${HADOOP_HOME}/bin/yarn jar wordcountimproved.jar WordCountImproved wordcount-2 output-2
+
+head output-1/part-r-00000
+head output-2/part-r-00000
+
