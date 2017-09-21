c8ff3c98a62e7832ee4e6662de78df27190eb3e6
Muhammad Raihan Muhaimin <mmuhaimi@sfu.ca>
2017-09-20 17:43:08 -0700

    Finish reddit avg

--- docker_experiment/1B/RedditAvg/RedditAverage.java
+++ docker_experiment/1B/RedditAvg/RedditAverage.java
@@ -1,8 +1,10 @@
 // adapted from https://hadoop.apache.org/docs/current/hadoop-mapreduce-client/hadoop-mapreduce-client-core/MapReduceTutorial.html
  
 // package ca.sfu.whatever;
- 
+
 import java.io.IOException;
+import java.io.DataOutput;
+import java.io.DataInput;
 import java.util.StringTokenizer;
 import java.lang.Object;
 import java.text.Normalizer;
@@ -13,8 +15,10 @@ import java.text.BreakIterator;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -28,13 +32,60 @@ import org.apache.hadoop.util.ToolRunner;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
  
 public class RedditAverage extends Configured implements Tool {
  
-    public static class SubRedditScoreMapper
-    extends Mapper<LongWritable, Text, Text, LongWritable>{
+	/**
+	 * A Writable that represents a pair of long values.
+	 */
+	public static class LongPairWritable implements Writable {
+		private long a;
+		private long b;
+		private LongWritable writ = new LongWritable();
+
+		public LongPairWritable(long a, long b) {
+			this.a = a;
+			this.b = b;
+		}
+
+		public LongPairWritable() {
+			this(0, 0);
+		}
+		
+		public long get_0() {
+			return a;
+		}
+		public long get_1() {
+			return b;
+		}
+		public void set(long a, long b) {
+			this.a = a;
+			this.b = b;		
+		}
+
+		public void write(DataOutput out) throws IOException {
+			writ.set(a);
+			writ.write(out);
+			writ.set(b);
+			writ.write(out);
+		}
+		public void readFields(DataInput in) throws IOException {
+			writ.readFields(in);
+			a = writ.get();
+			writ.readFields(in);
+			b = writ.get();
+		}
+		
+		public String toString() {
+			return "(" + Long.toString(a) + "," + Long.toString(b) + ")";
+		}
+
+	}
+	
+	public static class SubRedditScoreMapper
+    extends Mapper<LongWritable, Text, Text, LongPairWritable>{
  
-        private static LongWritable pageHit;
         private Text word = new Text();
  
         @Override
@@ -43,26 +94,47 @@ public class RedditAverage extends Configured implements Tool {
             ObjectMapper json_mapper = new ObjectMapper();
             JsonNode data = json_mapper.readValue(value.toString(), JsonNode.class);
             word.set(data.get("subreddit").textValue());
-            context.write(word, new LongWritable(data.get("score").longValue()));
+            LongPairWritable pair = new LongPairWritable();
+            pair.set(1, data.get("score").longValue());
+            context.write(word, pair);
         }
     }
  
-    public static class MaxPageCountReducer
-    extends Reducer<Text, LongWritable, Text, LongWritable> {
-        private LongWritable result = new LongWritable();
+    public static class SubRedditScoreReducer
+    extends Reducer<Text, LongPairWritable, Text, DoubleWritable> {
+        private DoubleWritable result = new DoubleWritable();
  
         @Override
-        public void reduce(Text key, Iterable<LongWritable> values,
+        public void reduce(Text key, Iterable<LongPairWritable> values,
                 Context context
                 ) throws IOException, InterruptedException {
-            long max = 0;
-            for (LongWritable val : values) {
-                if ( val.get() >= max) {
-                    max = val.get();
-                }
+            long count = 0;
+            long sum = 0;
+            for (LongPairWritable val : values) {
+            	count += val.get_0();
+            	sum += val.get_1();
             }
-            result.set(max);
+            result.set((double)sum/count);
             context.write(key, result);
+        }
+    }
+    
+    public static class SubRedditScoreCombiner
+    extends Reducer<Text, LongPairWritable, Text, LongPairWritable> {
+
+        @Override
+        public void reduce(Text key, Iterable<LongPairWritable> values,
+                Context context
+                ) throws IOException, InterruptedException {
+            long count = 0;
+            long sum = 0;
+            for (LongPairWritable val : values) {
+            	count += val.get_0();
+            	sum += val.get_1();
+            }
+            LongPairWritable pair = new LongPairWritable();
+            pair.set(count, sum);
+            context.write(key, pair);
         }
     }
 
@@ -80,11 +152,11 @@ public class RedditAverage extends Configured implements Tool {
         job.setInputFormatClass(TextInputFormat.class);
  
         job.setMapperClass(SubRedditScoreMapper.class);
-        job.setCombinerClass(MaxPageCountReducer.class);
-        job.setReducerClass(MaxPageCountReducer.class);
+        job.setCombinerClass(SubRedditScoreCombiner.class);
+        job.setReducerClass(SubRedditScoreReducer.class);
  
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(LongWritable.class);
+        job.setOutputValueClass(LongPairWritable.class);
         job.setOutputFormatClass(TextOutputFormat.class);
         TextInputFormat.addInputPath(job, new Path(args[0]));
         TextOutputFormat.setOutputPath(job, new Path(args[1]));
--- docker_experiment/1B/RedditAvg/run_lab.sh
+++ docker_experiment/1B/RedditAvg/run_lab.sh
@@ -1,13 +1,13 @@
 rm -rf output-*
 rm -rf wordcount-*
 rm -r *.class
-rm -r *.jar
+rm -r redditaverage.jar
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export HADOOP_HOME=/usr/shared/CMPT/big-data/hadoop-2.6.0
 
 ${JAVA_HOME}/bin/javac -classpath `${HADOOP_HOME}/bin/hadoop classpath` RedditAverage.java
 ${JAVA_HOME}/bin/jar cf redditaverage.jar RedditAverage*.class
-# ${HADOOP_HOME}/bin/yarn jar redditaverage.jar RedditAverage -D mapreduce.job.reduces=0 pagecounts-1/ output-0
-# ${HADOOP_HOME}/bin/yarn jar redditaverage.jar RedditAverage pagecounts-1/ output-1
-# cat output-0/part-m-00000
-# cat output-1/part-r-00000
+#${HADOOP_HOME}/bin/yarn jar redditaverage.jar RedditAverage -D mapreduce.job.reduces=0 -libjars ${PWD}/jackson-annotations-2.8.10.jar,${PWD}/jackson-databind-2.8.10.jar,${PWD}/jackson-core-2.8.10.jar reddit-1/ output-0
+${HADOOP_HOME}/bin/yarn jar redditaverage.jar RedditAverage -libjars ${PWD}/jackson-annotations-2.8.10.jar,${PWD}/jackson-databind-2.8.10.jar,${PWD}/jackson-core-2.8.10.jar reddit-1/ output-1
+#cat output-0/part-m-00000
+cat output-1/part-r-00000
