7fd4dd6bdf0be07de1abdb95d6fa0b7733c0dffb
Muhammad Muhaimin <muhammad.r.muhaimin@gmail.com>
2017-09-28 14:55:56 -0700

    Remove sortby

--- 2A/runhome.sh
+++ 2A/runhome.sh
@@ -5,5 +5,5 @@ unzip wordcount-1.zip
 rm wordcount-1.zip
 spark-submit wordcount-improved.py wordcount-1 output-1
 rm -r wordcount-1
-cat output-1/part-00000
+cat output-1/part-* | tail -n 15
 grep -i "^better" output-1/part*
--- 2A/wordcount-improved.py
+++ 2A/wordcount-improved.py
@@ -14,8 +14,8 @@ assert sc.version >= '2.2'  # make sure we have Spark 2.2+
 
 
 def words_once(line):
-    wordsep = re.compile(r'[%s\s]+' % re.escape(string.punctuation))
-    for w in wordsep.split(line):
+    word_separator = re.compile(r'[%s\s]+' % re.escape(string.punctuation))
+    for w in word_separator.split(line):
         w = unicodedata.normalize('NFD', w)
         w = w.lower()
         yield (w, 1)
@@ -35,5 +35,5 @@ words = text.flatMap(words_once)
 words = words.filter(lambda x: x != '')
 wordcount = words.reduceByKey(operator.add)
 
-outdata = wordcount.sortBy(get_key).map(output_format)
+outdata = wordcount
 outdata.saveAsTextFile(output)
