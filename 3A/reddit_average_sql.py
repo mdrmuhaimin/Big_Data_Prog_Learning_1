cca91706e84b1ab3ebd452db24b0b86047652139
Muhammad Muhaimin <muhammad.r.muhaimin@gmail.com>
2017-10-12 16:37:44 -0700

    Add schema

--- 3A/reddit_average_sql.py
+++ 3A/reddit_average_sql.py
@@ -11,13 +11,18 @@ output = sys.argv[2]
 
 
 def main():
-    comments = spark.read.json(inputs)
+    # The schema is encoded in a string.
+    schemaString = "subreddit score"
+    fields = [types.StructField(field_name, types.StringType(), True) for field_name in schemaString.split()]
+    schema = types.StructType(fields)
+    comments = spark.read.json(inputs, schema)
     comments.createOrReplaceTempView('comments')
     averages = spark.sql("""
         SELECT subreddit, AVG(score)
         FROM comments
         GROUP BY subreddit
     """)
+
     averages.show()
     averages.write.save(output, format='json', mode='overwrite')
 
